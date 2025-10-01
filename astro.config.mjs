c8bc983c7fe562d10b70857e209a6917a90f6499
ROL4ND909 <mail@rolandfranke.nl>
2025-10-01 11:24:55 +0200

    Update Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rol4nd909.github.io',
   base: '/fm-blog-preview-card/',
-  integrations: [icon(), tailwind({
+  integrations: [tailwind({
     applyBaseStyles: false,
     config: './tailwind.config.mjs',
     nesting: true,
